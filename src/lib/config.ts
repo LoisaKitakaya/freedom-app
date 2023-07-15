f25f20225ad8db068fa3a0a915e988325183c6c5
Freedom Loisa <kitakayaloisa@gmail.com>
2023-07-15 15:26:33 +0300

    minor changes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,5 +2,5 @@ import { dev } from '$app/environment'
 
 export const title = 'Loisa Kitakaya'
 export const description = 'My Portfolio, Blog, and GitHub activity all in one place.'
-export const url = dev ? 'http://localhost:5173' : 'https://freedomloisa.vercel.app'
+export const url = dev ? 'http://localhost:5173' : 'https://loisakitakaya.vercel.app'
 export const github = 'https://github.com/LoisaKitakaya/freedom-app/tree/main'
