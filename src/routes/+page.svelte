8d11a31d5750fe91ff4302778cf625ff2d057a2f
Freedom Loisa <kitakayaloisa@gmail.com>
2023-07-15 09:22:16 +0300

    created home page, showing recent github activity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,68 @@
-<script>
+<script lang="ts">
 	import * as config from '$lib/config'
+	import { json } from '@sveltejs/kit'
+	import moment from 'moment'
+
+	export let data
+
+	const { events } = data
+
+	const getName = (name: string) => {
+		const parts = name.split('/')
+
+		return parts[parts.length - 1]
+	}
+
+	const format = (data: any) => {
+		return JSON.stringify(data, null, 2)
+	}
 </script>
 
 <svelte:head>
 	<title>{config.title} | Home</title>
 </svelte:head>
+
+<h1 class="text-lg mb-8 text-center underline">My recent <strong>GitHub</strong> activity</h1>
+
+<section class="w-3/5 mx-auto">
+	{#each events as event (event.id)}
+		<div class="py-4 px-10 mb-4 flex justify-start items-center gap-4">
+			<div class="tooltip" data-tip={event.actor.login}>
+				<a href={event.actor.url}
+					><div class="avatar">
+						<div class="w-12 rounded-full">
+							<img src={event.actor.avatar_url} alt={event.actor.display_login} />
+						</div>
+					</div></a
+				>
+			</div>
+			<div>
+				<div class="flex justify-start items-center gap-4 mb-2 flex-wrap">
+					<p>
+						<span class="font-semibold">Repo:</span>
+						<a href={event.repo.url} class="link link-hover">{getName(event.repo.name)}</a>
+					</p>
+					<p>
+						<span class="font-semibold">Status:</span>
+						{event.public ? 'Public' : 'Private'}
+					</p>
+					<p><span class="font-semibold">Event:</span> {event.type}</p>
+				</div>
+				<p><span class="font-semibold">Last:</span> {moment(event.created_at).fromNow()}</p>
+			</div>
+		</div>
+		<details class="collapse bg-base-200">
+			<summary class="collapse-title"
+				>Show Payload - Click to open/close</summary
+			>
+			<div class="collapse-content">
+				<pre class="overflow-x-auto">
+					<code>
+						{format(event.payload)}
+					</code>
+				</pre>
+			</div>
+		</details>
+		<div class="divider">🔥</div>
+	{/each}
+</section>
---
+++ src/routes/+page.ts
@@ -0,0 +1,19 @@
+import { okToken, githubUser } from '$lib/secrets'
+import { Octokit } from 'octokit'
+
+export const load = async () => {
+	const octokit = new Octokit({ auth: okToken })
+
+	const response = await octokit.request('GET /users/{username}/events?per_page=15', {
+		username: githubUser,
+		headers: {
+			'X-GitHub-Api-Version': '2022-11-28'
+		}
+	})
+
+    console.log(response.data.slice(0, 10))
+
+	return {
+		events: response.data
+	}
+}
