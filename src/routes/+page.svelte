14060297259f4191351e8ec79851e639ead8ef03
Freedom Loisa <kitakayaloisa@gmail.com>
2024-03-30 09:53:22 +0300

    minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,20 @@
 	import { Button } from 'flowbite-svelte';
 </script>
 
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
 <section class="min-h-screen p-4 dark:text-white" id="banner">
 	<div class="m-auto flex h-fit w-fit flex-col items-center gap-8 pt-64">
 		<h1 class="text-3xl font-semibold">
 			I am a <strong>fullstack developer</strong> & <strong>graphic designer</strong>
 		</h1>
 		<span class="text-lg italic">I Design & Develop Solutions You'll Love</span>
-		<Button href="/work" color="light" class="bg-gray-200 text-black dark:bg-gray-600 dark:text-white"
-			>Portfolio</Button
+		<Button
+			href="/work"
+			color="light"
+			class="bg-gray-200 text-black dark:bg-gray-600 dark:text-white">Portfolio</Button
 		>
 	</div>
 </section>
