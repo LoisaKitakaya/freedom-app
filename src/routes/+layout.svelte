a117ef021b71f74492c957629a918544f581a214
Freedom Loisa <kitakayaloisa@gmail.com>
2024-03-30 07:34:18 +0300

    page layout for desktop

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.pcss';
+
+	import NavDrawer from '$lib/components/NavDrawer.svelte';
+</script>
+
+<NavDrawer />
+
+<main class="absolute right-0 p-4" id="main-body">
+	<slot />
+</main>
+
+<style>
+	#main-body {
+		width: 80%;
+	}
+</style>
