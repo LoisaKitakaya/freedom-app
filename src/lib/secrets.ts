fedf9aa30ec4bcd6af9478f8b03ca1bdbc77f285
Freedom Loisa <kitakayaloisa@gmail.com>
2024-04-01 06:44:45 +0300

    app secrets

---
+++ src/lib/secrets.ts
@@ -0,0 +1,5 @@
+import { PASSWORD, GMAIL_ADDRESS, GMAIL_PASSWORD } from '$env/static/private';
+
+export const password = PASSWORD;
+export const gmail_address = GMAIL_ADDRESS;
+export const gmail_password = GMAIL_PASSWORD;
