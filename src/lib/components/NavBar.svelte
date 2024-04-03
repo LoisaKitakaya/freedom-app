b6d57f087b9cbed7ee2c7868f2841986e56d8cf6
Freedom Loisa <kitakayaloisa@gmail.com>
2024-04-03 07:54:54 +0300

    minor theme changes for mobile friendly view

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
+
+	$: activeUrl = $page.url.pathname;
+
+	let activeClass = 'text-orange-500 underline';
+	let nonActiveClass = 'text-gray-700 dark:text-white hover:text-orange-500 hover:underline';
 </script>
 
-<div class="sticky top-0 z-10 flex min-h-[40px] items-center justify-center border-b md:hidden">
-	<Navbar class="w-full">
+<div class="sticky top-0 z-10 flex min-h-[40px] items-center justify-center md:hidden">
+	<Navbar class="w-full dark:bg-gray-700">
 		<NavBrand href="/">
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 				>Loisa Kitakaya</span
@@ -14,8 +20,8 @@
 			<DarkMode />
 			<NavHamburger />
 		</div>
-		
-		<NavUl>
+
+		<NavUl {activeUrl} {activeClass} {nonActiveClass}>
 			<NavLi href="/">Home</NavLi>
 			<NavLi href="/services">Services</NavLi>
 			<NavLi href="/skills">Skills</NavLi>
