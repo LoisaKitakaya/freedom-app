41dec82d3dac8980fe8539d6c86fb734e168ade9
Freedom Loisa <kitakayaloisa@gmail.com>
2024-04-09 05:04:40 +0300

    updated app to not show empty work

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -4,8 +4,20 @@
 	import webData from '$lib/data/web_dev_work.json';
 	import designData from '$lib/data/graphic_design_work.json';
 
-	const webDevWork = webData.projects;
-	const designWork = designData.projects;
+	interface Project {
+		name: string;
+		description: string;
+		link: string;
+		images: Image[];
+	}
+
+	interface Image {
+		src: string;
+		alt: string;
+	}
+
+	const webDevWork: Project[] = webData.projects;
+	const designWork: Project[] = designData.projects;
 </script>
 
 <svelte:head>
@@ -15,58 +27,70 @@
 <section class="min-h-screen w-full p-4 text-gray-900 dark:text-white">
 	<Tabs style="underline" contentClass="!bg-white dark:!bg-gray-700 md:p-4 p-0 md:mt-20 mt-8">
 		<TabItem open title="Websites & Web-apps">
-			<div class="grid grid-cols-1 gap-16">
-				{#each webDevWork as work}
-					<div class="mx-auto flex w-4/5 flex-row flex-wrap gap-8 md:flex-nowrap">
-						<Card class="!p-0">
-							<Carousel images={work.images} duration={3900} let:Indicators class="mx-auto">
-								<Indicators />
-							</Carousel>
-						</Card>
+			{#if webDevWork.length !== 0 || webDevWork.length > 1}
+				<div class="grid grid-cols-1 gap-16">
+					{#each webDevWork as work}
+						<div class="mx-auto flex w-4/5 flex-row flex-wrap gap-8 md:flex-nowrap">
+							<Card class="!p-0">
+								<Carousel images={work.images} duration={3900} let:Indicators class="mx-auto">
+									<Indicators />
+								</Carousel>
+							</Card>
 
-						<div class="flex w-full flex-col justify-between">
-							<div>
-								<h1 class="mb-4 text-2xl font-semibold">{work.name}</h1>
+							<div class="flex w-full flex-col justify-between">
+								<div>
+									<h1 class="mb-4 text-2xl font-semibold">{work.name}</h1>
 
-								<p>
-									{work.description}
-								</p>
-							</div>
+									<p>
+										{work.description}
+									</p>
+								</div>
 
-							<div class="mt-4 w-full">
-								<Button href={work.link} class="w-full">Preview</Button>
+								<div class="mt-4 w-full">
+									<Button href={work.link} class="w-full">Preview</Button>
+								</div>
 							</div>
 						</div>
-					</div>
-				{/each}
-			</div>
+					{/each}
+				</div>
+			{:else}
+				<div class="py-20 text-center">
+					<p class="text-2xl font-semibold">Coming Soon!</p>
+				</div>
+			{/if}
 		</TabItem>
 		<TabItem title="Design Work">
-			<div class="grid grid-cols-1 gap-16">
-				{#each designWork as work}
-					<div class="mx-auto flex w-4/5 flex-row flex-wrap gap-8 md:flex-nowrap">
-						<Card class="!p-0">
-							<Carousel images={work.images} duration={3900} let:Indicators class="mx-auto">
-								<Indicators />
-							</Carousel>
-						</Card>
+			{#if designWork.length !== 0 || designWork.length > 1}
+				<div class="grid grid-cols-1 gap-16">
+					{#each designWork as work}
+						<div class="mx-auto flex w-4/5 flex-row flex-wrap gap-8 md:flex-nowrap">
+							<Card class="!p-0">
+								<Carousel images={work.images} duration={3900} let:Indicators class="mx-auto">
+									<Indicators />
+								</Carousel>
+							</Card>
 
-						<div class="flex w-full flex-col justify-between">
-							<div>
-								<h1 class="mb-4 text-2xl font-semibold">{work.name}</h1>
+							<div class="flex w-full flex-col justify-between">
+								<div>
+									<h1 class="mb-4 text-2xl font-semibold">{work.name}</h1>
 
-								<p>
-									{work.description}
-								</p>
-							</div>
+									<p>
+										{work.description}
+									</p>
+								</div>
 
-							<div class="mt-4 w-full">
-								<Button href={work.link} class="w-full">Preview</Button>
+								<div class="mt-4 w-full">
+									<Button href={work.link} class="w-full">Preview</Button>
+								</div>
 							</div>
 						</div>
-					</div>
-				{/each}
-			</div>
+					{/each}
+				</div>
+			{:else}
+				<div class="py-20 text-center">
+					<p class="text-2xl font-semibold">Coming Soon!</p>
+				</div>
+			{/if}
 		</TabItem>
 	</Tabs>
 </section>
